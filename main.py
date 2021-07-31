84142e305c7579d8af310ba0e44aeba6e709acba
WiktorBK <wiktorsebbozek@gmail.com>
2021-07-31 13:50:32 +0200

    doskonale

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ def sito_erastotenesa(n):
                 break
         else:
             liczby_pierwsze.append(liczba)
-    return liczby_pierwsze
+    return f"\nliczby pierwsze w zakresie 2 - {n}: \n{liczby_pierwsze}\n"
+print(sito_erastotenesa(100))
 
 #####################################################################
 
@@ -61,8 +62,8 @@ def ciag_fibonacciego(n):
     for i in range(n - 2):
         result.append(result[i] + result[(i + 1)])
 
-    return result
-
+    return f"Pierwsze {n} liczb ciagu fibonacciego to: \n{result}\n"
+print(ciag_fibonacciego(15))
 ########################################################################
 
 #Sprawdza czy podana wartosc jest palindromem
@@ -70,8 +71,11 @@ def czy_palindrom(s):
     s = str(s)
     s = s.lower()
     s = s.replace(" ", "")
-    return s == s[::-1]
-
+    if s == s[::-1]:
+        return f"{s} jest palindromem\n"
+    else:
+        return f"{s} nie jest palindromem\n"
+print(czy_palindrom("kajak"))
 ########################################################################
 
 #Sprawdza czy podane dwie wartosci sa anagramami
@@ -79,12 +83,16 @@ def czy_palindrom(s):
 def czy_anagram(s1, s2):
     s1, s2 = str(s1), str(s2)
     s1 , s2 = s1.replace(" ", "").lower(), s2.replace(" ", "").lower()
-    return sorted(s1) == sorted(s2)
-
+    if sorted(s1) == sorted(s2):
+        return f"{s1} i {s2} są anagramami\n"
+    else:
+        return f"{s1} i {s2} nie są anagramami\n"
+print(czy_anagram("niedziela", "dzielenia"))
 ########################################################################
 
 #rozklada liczbe na czynniki pierwsze
 def rozloz(n):
+    i = n
     czynniki = []
     k = 2
     while n != 1:
@@ -92,8 +100,8 @@ def rozloz(n):
             n //= k
             czynniki.append(k)
         k +=1
-    return czynniki
-
+    return f"czynniki pierwsze liczby {i} to: \n{czynniki}\n"
+print(rozloz(15))
 ###############################################################################
 
 #szyfruje napis szyfrem cezara
@@ -103,6 +111,28 @@ def szyfr_cezara(s, n):
     for i in range(len(s)):
         liczba = ord(s[i])  # zapisuje dana literke z napisu w postaci liczby
         szyfr += chr(liczba + n)
-    return szyfr
-
+    return f"Szyfr cezara słowa {s} po przesunieciu o {n} to: \n{szyfr}\n"
+print(szyfr_cezara("ABDSDFHGASDGSD", 6))
 ###############################################################################
+
+#Zwraca dzielniki danej liczby
+def dzielniki(n):
+    dzielniki = []
+    for a in range(1, (int(n/2)) + 1):
+        if n % a == 0:
+            dzielniki.append(a)
+    return dzielniki
+print("Dzielnikami liczby 1024 oprócz jej samej są:")
+print(dzielniki(1024))
+################################################################################
+
+#Sprawdza czy podana liczba jest doskonała z wykorzystaniem funkcji dzielniki()
+def czy_doskonala(n):
+    suma = 0
+    for i in dzielniki(n):
+        suma += i
+    if suma == n:
+        return f"\nLiczba {n} jest liczba doskonala"
+    else:
+        return f"\nLiczba {n} nie jest liczba doskonala"
+print(czy_doskonala(28))
